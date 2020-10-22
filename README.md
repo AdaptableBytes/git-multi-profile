776341c9c24cff5c382bc0a0e534a2e244da6a37
adaptablebytes <info@adaptablebytes.com>
2020-10-21 20:36:45 -0700

    add code

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
-# git-multi-profile
-Easily and automatically use the correct SSH key and user profile for multiple git accounts
+## git-multi-profile
+
+Automatically use the correct SSH key and user profile for git operations without having to remember any extra steps. Once configured, it just works. 
+
+No more cases of mistaken identity or "Permission Denied" when trying to access a repo when you have multiple git accounts.
+
+Works on MacOS/Linux
+
+-----------------
+Install
+-----------------
+```shell
+npm i -g git-multi-profile
+```
+
+📌 Note: Global install is important here since this will be used outside the scope of any individual NodeJS project
+
+-----------------
+Set up
+-----------------
+```shell
+# Download shell script
+curl https://raw.githubusercontent.com/AdaptableBytes/git-multi-profile/main/git-init
+
+# Make available as shell command
+sudo cp git-init /usr/local/bin && sudo chmod +x /usr/local/bin/git-init
+```
+
+
+-----------------
+Configure
+-----------------
+Create config file in user home directory with your account info
+
+`~/.git-profiles.json`
+```
+{
+  "personal": {
+    "name": "Name or Username",
+    "email": "your-email@example.com",
+    "hostprefix": "personal"
+  },
+  "work": {
+    "name": "Name or Username",
+    "email": "your-work-email@example.com",
+    "hostprefix": "work"
+  }  
+}
+```
+
+**Add SSH Keys**
+
+If you don't already have SSH keys configured for your git accounts, create an SSH key for each profile you set up in `~/git-profiles.json`
---
+++ index.js
@@ -0,0 +1,85 @@
+const path = require('path');
+const os = require('os');
+const gitconfig = require('gitconfig');
+const gitUrlParse = require('git-url-parse');
+const commandLine = require('./lib/parse-command-line');
+const FileHelper = require('./lib/utilities/file-helper');
+const ProfileManager = require('./lib/profile-manager');
+
+function quit(exitCode = 0) {
+  process.exit(exitCode);
+}
+
+function formatUrl(repo, profile) {
+  let hostname = profile.hostprefix ? `${profile.hostprefix}.` : '';
+  let repoPath = repo.pathname.startsWith('/') ? repo.pathname.substr(1, repo.pathname.length - 1) : repo.pathname;
+  return `${repo.user}@${hostname}${repo.resource}:${repoPath}`;
+}
+
+module.exports = async function run() { 
+  const args = commandLine();
+  const homedir = os.homedir();
+
+  const gitProfilesFile = path.join(homedir, ".git-profiles.json");
+  const exists = await FileHelper.fileExists(gitProfilesFile);
+  if (exists === false) {
+    console.error(`${gitProfilesFile} not found`);
+    return quit(2);
+  }
+  let profileJson = null;
+  try {
+    profileJson = await FileHelper.readJsonFile(gitProfilesFile);
+  } catch(e) {
+    console.error(`Unable to parse JSON file ${gitProfilesFile}`);
+    return quit(3);
+  }
+  const profileManager = new ProfileManager(profileJson);
+
+  if (args["list-profiles"] === true) {
+    console.log(JSON.stringify(profileManager.listProfiles()));
+    return quit(0);
+  }
+
+  if (!args.profile) {
+    console.error("--profile is required");
+    return quit(1);
+  }
+
+  const profile = profileManager.getProfile(args.profile);
+  if (!profile) {
+    console.error(`No profile found for ${args.profile}`);
+    return quit(2);
+  }
+
+  let modifiedRepo = null;
+  let gitInfo = null;
+
+  if (args.repo) {
+    gitInfo = gitUrlParse(args.repo);
+    modifiedRepo = formatUrl(gitInfo, profile);
+  }
+
+  if (args['format-url'] === true) {
+    if (!args.repo) {
+      console.error("Missing repo argument");
+      return quit(2);
+    }    
+    console.log(modifiedRepo);
+    return process.exit(0);
+  } else if (args['get-repo-name'] === true) {
+    if (!args.repo) {
+      console.error("Missing repo argument");
+      process.exit(2);
+    }
+    console.log(gitInfo.name);
+    return process.exit(0);
+  } else if (args['set-local-profile'] === true) {
+    //set user name and email
+    await gitconfig.set({
+      'user.email': profile.email,
+      'user.name': profile.name
+    }, {location: 'local'});
+
+    return process.exit(0);
+  }
+};
